5b25256201c0ea301ba7370ee79b624edda7d854
chenmingwei <chen_mingwei@126.com>
2020-06-27 06:39:06 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.default')
 @section('title', '首页')
 @section('content')
-  <div class="jumbotron">
+  @if(Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user'=> Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
         你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
@@ -15,4 +29,5 @@
       </p>
       @endif
   </div>
+  @endif
 @stop
